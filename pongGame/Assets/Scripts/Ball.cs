143d25c02cd77ad72a855283ff19000ed45e271d
Tatiana Oshiro Kobayashi <tatianakobayashi@hotmail.com>
2024-01-09 13:56:17 -0400

    Movimento da bola

---
+++ pongGame/Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 randomDirection = new Vector2(Random.value, Random.value);
+        // OBS: Força está variando
+        GetComponent<Rigidbody2D>().AddForce(randomDirection * - 600f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
